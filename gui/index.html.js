81582f91a2d3d089c428714daaefea0347543248
Shae <a.thomas.vc@gmail.com>
2023-07-09 19:26:30 -0700

    Refactor; add buttons to sort and clear mods

--- gui/index.html.js
+++ gui/index.html.js
@@ -1,35 +1,45 @@
+globalThis.bt = {};
+
 // top bar buttons
-globalThis.new_window = function() {
+bt.new_window = function() {
     new $guapi.Window().duplicate();
-}
-globalThis.tutorial = function() {
+};
+bt.tutorial = function() {
     for (let m of ["thanks", "infotutor", "infotutor.2", "infotutor.3"])
         $l.alert(`[mg.${m};`);
-}
-globalThis.reload = function() {
+};
+bt.reload = function() {
     if ($l.confirm("[mg.confirm.rel;")) location.reload();
-}
+};
 
 // mods-dir bar buttons
-globalThis.change_dir = async function() {
+bt.change_dir = async function() {
     set_all_by_attr("d_w_chdir", "disabled", true);
     let mdir = await $g.open_dialog($wid, "folder", {
         directory: await $g.m.default_directory(),
     });
     if (mdir !== null) $e.mod_dir.value = mdir;
     set_all_by_attr("d_w_chdir", "disabled", false);
-}
-globalThis.find_mods = async function() {
+};
+bt.find_mods = async function() {
     set_all_by_attr("d_w_fmods", "disabled", true);
     $mod_h.get_insert_mods($e.mod_container, $e.mod_dir.value);
     set_all_by_attr("d_w_fmods", "disabled", false);
+};
+
+// mods container container buttons
+bt.sort_mods = function() { $mod_h.sort($e.mod_container, "name"); };
+bt.clear_mods = function() {
+    if (!confirm("[mg.confirm.cmods;")) return;
+    for (let m of [...$e.mod_container.getElementsByTagName("li")])
+        m.remove();
 }
 
 // details box close button
-globalThis.close_details = function() {
+bt.close_details = function() {
     $e.details.contentWindow.location.replace("about:blank");
     $e.details_container.style.display = "none";
-}
+};
 
 // upon ready
 $g.and_dom_ready.then(async function() {
