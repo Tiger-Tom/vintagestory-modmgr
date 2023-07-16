7b85fd76878547675b5cf7c25abcce76d998cf0e
Shae <a.thomas.vc@gmail.com>
2023-07-16 03:29:29 -0700

    Made lock obtaining cancellable

--- gui/guapi/submodules/lock.js
+++ gui/guapi/submodules/lock.js
@@ -6,12 +6,14 @@ export default class {
         /* if wait is truthy, then we return a promise and check every {wait} seconds if the lock is available */
         /* otherwise, returns a promise that contains a boolean of whether or not we obtained the lock */
         if (!wait) return $bridge.lock_obtain(this.id);
-        return new Promise(resolve => {
+        let p = new Promise((resolve,reject) => {
             async function check() {
                 if (await $bridge.lock_obtain(id)) resolve();
+                else if (p.__cancelled) reject(); 
                 else setTimeout(check, wait*1000);
             }; check();
-        });
+        }); p.cancel = function() { p.__cancelled = true; };
+        return p;
     }
     async release() { return await $bridge.lock_release(this.id); }
 };
